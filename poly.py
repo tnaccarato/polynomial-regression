a077ffb6d3a74df8be497548d4e770a4bc86a307
Tom Naccarato <tcarlnaccarato@gmail.com>
2022-11-07 16:07:03 +0000

    Add read_csv function

--- poly.py
+++ poly.py
@@ -1,1 +1,24 @@
+# Imports
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+# Functions
+def read_csv():
+    '''Reads the CSV file and returns as a pandas dataframe'''
+    return pd.read_csv("covid-vaccination-vs-death_ratio.csv")
+
+
+# Declares a new dataframe for storing UK data (1)
+df = read_csv()
+UK_df = df[df.country == "The United Kingdom"]
+
+'''
+References
+
+Used data from here:
 # https://www.kaggle.com/datasets/sinakaraji/covid-vaccination-vs-death?resource=download
+
+'''
